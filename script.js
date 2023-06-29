4fec9ac11920e57edec7adbea96ee178e95bcf4e
Priyam8878 <112754436+Priyam8878@users.noreply.github.com>
2023-06-29 20:38:42 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,23 @@
 //your JS code here. If required.
+let res = document.getElementById("output")
+function greetings() {
+	return new Promise ((resolve,reject) => {
+		setTimeout(() => {
+			resolve("Hello, world")
+		})
+	})
+	
+}
+unction greetings() {
+	return new Promise ((resolve,reject) => {
+		setTimeout(() => {
+			resolve("Hello, world")
+		})
+	})
+	
+}
+
+let p = greetings().then((value) => {
+    console.log(value)
+})
+res.innerHTML = p;
